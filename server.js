08c64a90e26cb96a4e0923691bc63ac91853c763
Barath Tirumala <barath.tirumala@gmail.com>
2020-01-03 21:58:10 -0500

    add check safe for refresh routing error

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const app = express();
 const path = require("path");
 app.use(express.static(path.join(__dirname, "/build/")));
 
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build//index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+
 // Connect Database
 connectDB()
 
