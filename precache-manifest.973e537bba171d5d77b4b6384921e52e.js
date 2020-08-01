0e5108204c920af02058e3e9deb49fd7f62b8496
Barath Tirumala <barath.tirumala@gmail.com>
2020-08-01 12:28:20 -0400

    Updates

---
+++ precache-manifest.973e537bba171d5d77b4b6384921e52e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b00bd4b894cf25d1d4297b8c72979a",
+    "url": "/NewsFeed/index.html"
+  },
+  {
+    "revision": "a715607d9d83ee3526b1",
+    "url": "/NewsFeed/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9e07e34361b3139ff272",
+    "url": "/NewsFeed/static/css/main.df7601b9.chunk.css"
+  },
+  {
+    "revision": "a715607d9d83ee3526b1",
+    "url": "/NewsFeed/static/js/2.6a44ef58.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/NewsFeed/static/js/2.6a44ef58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e07e34361b3139ff272",
+    "url": "/NewsFeed/static/js/main.580d1661.chunk.js"
+  },
+  {
+    "revision": "07cba58702015c0558dd",
+    "url": "/NewsFeed/static/js/runtime-main.c06b402b.js"
+  }
+]);
